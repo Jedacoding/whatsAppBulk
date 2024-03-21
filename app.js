6e547b5b4ef8931b07d4c1621696410491f95751
Jedacoding <kerenjugague@gmail.com>
2024-03-21 10:56:49 +0800

    add info success and failed

--- app.js
+++ app.js
@@ -17,45 +17,65 @@ client.on("authenticated", () => {
 client.on("ready", () => {
     console.log("Client is ready!");
 
+    let success = 0;
+    let failed = 0;
+
     readExcel("./public/blast.xlsx").then(async (data) => {
         data.shift();
 
-        for (i in data) {
-            const name = data[i][0];
-            const phone = data[i][1];
+        await Promise.all(
+            data.map(async (item, i) => {
+                const name = item[1];
+                const phone = item[3];
 
-            if (phone) {
-                // change to local phone number
-                const convertedPhone = phone
-                    .replace(/-/g, "")
-                    .replace("0", "62");
-                const options = {
-                    weekday: "long",
-                    day: "numeric",
-                    month: "long",
-                    year: "numeric",
-                };
-                const currentDate = new Date().toLocaleDateString(
-                    "id-ID",
-                    options
-                );
+                if (phone) {
+                    // change to local phone number
+                    const convertedPhone = phone
+                        .replace(/-/g, "")
+                        .replace("0", "62");
+                    const options = {
+                        weekday: "long",
+                        day: "numeric",
+                        month: "long",
+                        year: "numeric",
+                    };
+                    const currentDate = new Date().toLocaleDateString(
+                        "id-ID",
+                        options
+                    );
 
-                const msg =
-                    "Assalamu'alaikum Warahmatullahi Wabarakatuh\n\nKepada Yth, Bapak/Ibu " +
-                    name +
-                    "\n\nTabe', dengan penuh kerendahan hati kami sampaikan, bahwa jadwal Bapak/Ibu untuk membawa hidangan buka puasa ke Mesjid Al Muraaqabah telah tiba.\n\nTanggal:\n" +
-                    currentDate +
-                    "\n\nWaktu:\nSetelah sholat ashar - menjelang maghrib\n\nTempat Penyaluran:\nPintu Utama Masjid\n\nAtas partisipasi Bapak/Ibu dalam hal tersebut, Kami pengurus/panitia masjid mengucapkan Jazakumullah Khairan Katsiran, semoga amal ibadah kita diterima disisi Allah subhanahu wa ta'ala, aamiin ya rabbal 'alamin\n\nWassalamu'alaikum Warahmatullahi Wabarakatuh\n*_Badan Eksekutif Mesjid Al-Muraaqabah_*";
-                const chatId = convertedPhone + "@c.us";
+                    const msg =
+                        "Assalamu'alaikum Warahmatullahi Wabarakatuh\n\nKepada Yth, Bapak/Ibu " +
+                        name +
+                        "\n\nTabe', dengan penuh kerendahan hati kami sampaikan, bahwa jadwal Bapak/Ibu untuk membawa hidangan buka puasa ke Mesjid Al Muraaqabah telah tiba.\n\nTanggal:\n" +
+                        currentDate +
+                        "\n\nWaktu:\nSetelah sholat ashar - menjelang maghrib\n\nTempat Penyaluran:\nPintu Utama Masjid\n\nAtas partisipasi Bapak/Ibu dalam hal tersebut, Kami pengurus/panitia masjid mengucapkan Jazakumullah Khairan Katsiran, semoga amal ibadah kita diterima disisi Allah subhanahu wa ta'ala, aamiin ya rabbal 'alamin\n\nWassalamu'alaikum Warahmatullahi Wabarakatuh\n*_Badan Eksekutif Mesjid Al-Muraaqabah_*";
+                    const chatId = convertedPhone + "@c.us";
 
-                const timeout = i * 5000;
+                    const timeout = i * 5000;
 
-                setTimeout(() => {
-                    client.sendMessage(chatId, msg);
-                    console.log(`Message Sent to Client ${name}`);
-                }, timeout);
-            }
-        }
+                    await new Promise((resolve) =>
+                        setTimeout(() => {
+                            try {
+                                client.sendMessage(chatId, msg);
+                                console.log(`Message Sent to Client ${name}`);
+                                success++;
+                            } catch (err) {
+                                console.log(err);
+                                console.log(
+                                    `Message Failed Sent to Client ${name}`
+                                );
+                                failed++;
+                            }
+
+                            resolve();
+                        }, timeout)
+                    );
+                }
+            })
+        );
+
+        console.log(`${success} success\n${failed} failed`);
     });
 });
 
